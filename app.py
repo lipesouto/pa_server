850488b92948951303ad740962a476d0c5539ead
Fellipe <fellipesouto@MacBook-Pro-de-JO.local>
2022-07-04 15:20:21 -0300

    teste de deploy automatico

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import os
 from flask import Flask, request
+import pymongo
 
 app = Flask(__name__)
+client = pymongo.MongoClient(
+    "mongodb+srv://admin:Palavra2022@palavraanimada.o2bklie.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database('pa_db')
+collection = db.get_collection('sessions')
 
-
-@app.route("/", methods=["GET"])
-def helloWorld():
-    return "testando a primeira rota!"
-
-
-@app.route("/teste", methods=["GET"])
-def teste():
-    return "testando a segunda rota!"
-
+@app.route("/saving_data_session", methods=["GET"])
+def savingDataSession():
+    dado_teste = {
+        "system": "Android",
+        "ajuda": [False, False, True],
+        "date_session": "20:48 22 June, 2022",
+        "nivel_jogado": "easy",
+        "obj_name": ["Cabine", "Pirulito", "Câmera"],
+        "resposta_certa": [False, False, True],
+        "resposta_errada": [True, True, False],
+        "tempo": [1.7150205373764038, 0.7294141054153442, 2.292156219482422],
+        "total_palavras": 16,
+        "total_pontos": 220
+    }
+    collection.insert_one(dado_teste)
+    return "Session salva com sucesso"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
